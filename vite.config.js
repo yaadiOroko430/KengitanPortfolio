4eb4ff14a4aa86705e68b5f935cb24c173becd76
Yaad <yaadiqaabato@gmail.com>
2024-11-30 23:49:41 +0300

    intial commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000
+  },
+  build: {
+    outDir: 'dist' // Explicitly set the output directory to 'dist'
   }
-})
+});
+
